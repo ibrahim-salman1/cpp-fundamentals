dafd5c370407d808c0c1179bc7f19ad922c9cb22
ibrahim-salman1 <ibrahim.salman2027@gmail.com>
2026-08-03 00:13:33 +0500

    Add files via upload

---
+++ Loops-and-Patterns/star-pattern.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int a, b;
+
+    for (a = 1; a <= 5; a++)
+    {
+        for (b = 1; b <= a; b++)
+        {
+            printf(" * ");
+        }
+
+        printf("\n");
+    }
+
+    getch();
+}
